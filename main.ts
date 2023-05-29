49b8edee80babf733d1498aa65fc853b2f5ead6e
esdraslov <103470150+esdraslov@users.noreply.github.com>
2023-05-29 13:51:53 -0300

    Atualizar 'main.ts'

--- main.ts
+++ main.ts
@@ -5,14 +5,23 @@
 //% width=500 height=500 icon="%5b" advanced="True"
 namespace ledLight_mb {
     export enum ledLightBrightness {
+        //% block="super light"
         superLight = 255,
+        //% block="light"
         light = 200,
+        //% block="dark"
         dark = 127,
+        //% block="super dark"
         superDark = 25,
+        //% block="off"
         off = 0
     }
     //% block
     export function setBrightness(brightness: number | ledLightBrightness){
         led.setBrightness(brightness)
+    }
+    //% block, advanced="true"
+    export function getLightBrightness(b: ledLightBrightness){
+        return b
     }
 }

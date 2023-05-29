3088c562ecd014f920de25575d8c4da9a5544498
esdraslov <103470150+esdraslov@users.noreply.github.com>
2023-05-29 13:47:02 -0300

    brightness

--- main.ts
+++ main.ts
@@ -1,4 +1,17 @@
-//% width=500 height=500 icon="%5b"
-namespace ledLight_mb {
 
+/**
+ * the led light libray for micro:bit
+ */
+//% width=500 height=500 icon="%5b" advanced="True"
+namespace ledLight_mb {
+    export enum ledLightBrightness {
+        superLight = 255,
+        light = 127,
+        dark = 25,
+        off = 0
+    }
+    //% block
+    export function setBrightness(brightness: number | ledLightBrightness){
+        led.setBrightness(brightness)
+    }
 }

137a10851b95f1af895e2d2b44b42237d0a40372
esdraslov <103470150+esdraslov@users.noreply.github.com>
2023-07-12 22:13:40 -0300

    Atualizar 'main.ts'

--- main.ts
+++ main.ts
@@ -7,6 +7,7 @@ enum LedLightColors {
  * the led light libray for micro:bit
  */
 //% width=500 height=500 icon="U+1F4AB" advanced=true blocks="normal config"
+//% groups=["light","color"]
 namespace ledLight_mb {
     export enum ledLightBrightness {
         //% block="super light"
@@ -21,14 +22,17 @@ namespace ledLight_mb {
         off = 0
     }
     //% block inlineInputMode = external
+    //% group="light"
     export function setBrightness(brightness: number | ledLightBrightness){
         led.setBrightness(brightness)
     }
     //% block
+    //% group="light"
     export function getLightBrightness(b: ledLightBrightness){
         return b
     }
     //% block
+    //% group="color"
     export function setColor(color: LedLightColors) {
         if (color == 255) {
             led.plotBarGraph(1, 2)
